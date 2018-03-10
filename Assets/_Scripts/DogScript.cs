1f5f71b12c3229b245cfac66edd26bc1362b65c8
David Rosales <anekin@mit.edu>
2018-03-09 21:21:35 -0500

    Added bark ammo and health to the Dog object

--- Assets/_Scripts/DogScript.cs
+++ Assets/_Scripts/DogScript.cs
@@ -7,15 +7,19 @@ public class DogScript : MonoBehaviour {
 	public float speed;
 	public float barkSpeed;
 
-	public int health;
-	public int ammo;
-
 	public GameObject barkPrefab;
+
+	private int maxHealth;
+	private int maxBarks;
+	private int health;
+	private int barks;
 
 	// Use this for initialization
 	void Start () {
+		maxHealth = 10;
+		maxBarks = 10;
 		health = 10;
-		ammo = 10;
+		barks = 10;
 	}
 
 	// Update is called once per frame
@@ -23,7 +27,9 @@ public class DogScript : MonoBehaviour {
 		move ();
 		rotate ();
 		if (Input.GetButtonDown ("Fire1")) {
-			shoot ();
+			if (barks > 0) {
+				shoot ();
+			}
 		}
 	}
 
@@ -45,11 +51,42 @@ public class DogScript : MonoBehaviour {
 	}
 
 	private void shoot(){
+		barks--;
 		GameObject bark = (GameObject)Instantiate (barkPrefab, new Vector3(transform.position.x, transform.position.y, 1), transform.rotation);
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		worldPos.z = 1;
 		Vector3 difference = worldPos - bark.transform.position;
 		difference.Normalize ();
 		bark.GetComponent<Rigidbody2D> ().AddForce (difference*barkSpeed);
+	}
+
+	public void gainBarks(int amount){
+		barks += amount;
+		if (barks > maxBarks) {
+			barks = maxBarks;
+		}
+	}
+
+	public void gainHealth(int amount){
+		health += amount;
+		if (health > maxHealth) {
+			health = maxHealth;
+		}
+	}
+
+	public int getBarks(){
+		return barks;
+	}
+
+	public int getMaxBarks(){
+		return maxBarks;
+	}
+
+	public int getHealth(){
+		return health;
+	}
+
+	public int getMaxHealth(){
+		return maxHealth;
 	}
 }
