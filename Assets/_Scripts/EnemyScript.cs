9b8057edb693e28d7ae3e4231de71ce16e2ab31e
TLiang518 <terranceliang01@gmail.com>
2018-03-12 00:19:37 -0400

    Added Enemy Spawning and simple AI
    need to figure out how to get hit by barks and how to attack player

---
+++ Assets/_Scripts/EnemyScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public int health;
+    private Transform target;
+    private float speed;
+
+    private float lastAttacked;
+    private float attackSpeed;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        speed = 0.5f;
+        attackSpeed = 1.0f;
+        lastAttacked = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateToPlayer();
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance < 1.0)
+        {
+            attack();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void rotateToPlayer()
+    {
+        Vector3 difference = target.position - transform.position;
+        float angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg + 90f;
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void attack()
+    {
+        // Attack once per second
+        if (Time.time - lastAttacked > attackSpeed)
+        {
+            //Attack logic here
+            lastAttacked = Time.time;
+        }
+        return;
+    }
+
+    private void loseHealth()
+    {
+        health -= 1;
+        if (health <= 0)
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
+
+}
+
---
+++ Assets/_Scripts/SpawnScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    private float spawnRate;
+    private float lastSpawn;
+
+    public GameObject kittenPrefab;
+    public GameObject catPrefab;
+    public GameObject lionPrefab;
+
+    private List<Vector3> spawnSpots;
+
+    // Use this for initialization
+	void Start () {
+        spawnRate = 5.0f;
+        lastSpawn = Time.time;
+        spawnSpots = new List<Vector3>();
+        spawnSpots.Add(new Vector3(-7.0f, 0, 0));
+        spawnSpots.Add(new Vector3(7.0f, 0, 0));
+        spawnSpots.Add(new Vector3(0, 5.0f, 0));
+        spawnSpots.Add(new Vector3(0, -5.0f, 0));
+    }
+
+    void spawnKitten()
+    {
+        Vector3 spawnLocation = spawnSpots[Random.Range(0, 4)];
+        GameObject kitten = (GameObject)Instantiate(kittenPrefab, spawnLocation, new Quaternion());
+    }
+
+    void spawnCat()
+    {
+        Vector3 spawnLocation = spawnSpots[Random.Range(0, 4)];
+        GameObject cat = (GameObject)Instantiate(catPrefab, spawnLocation, new Quaternion());
+    }
+
+    void spawnLion()
+    {
+        Vector3 spawnLocation = spawnSpots[Random.Range(0, 4)];
+        GameObject cat = (GameObject)Instantiate(lionPrefab, spawnLocation, new Quaternion());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Time.time - lastSpawn > spawnRate)
+        {
+            int val = Random.Range(0, 3);
+            if (val == 0)
+            {
+                spawnKitten();
+            }
+            else if (val == 1)
+            {
+                spawnCat();
+            }
+            else
+            {
+                spawnLion();
+            }
+            lastSpawn = Time.time;
+        }
+        
+	}
+}
