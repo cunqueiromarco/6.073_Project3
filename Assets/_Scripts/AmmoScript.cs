0c7655c9de60d334f83e4c4c606c0c69017f39cb
Marco Cunqueiro <cunqueim@mit.edu>
2018-03-18 14:55:49 -0400

    Ammo pickup + animation

---
+++ Assets/_Scripts/AmmoScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoScript : MonoBehaviour {
+
+	public int ammoRegen;
+
+	// Use this for initialization
+	void Start () {
+		ammoRegen = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			DogScript dogScript = collision.gameObject.GetComponent ("DogScript") as DogScript;
+			dogScript.gainBarks (3);
+			Object.Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/_Scripts/AmmoSpawnScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoSpawnScript : MonoBehaviour {
+
+	public float ammoSpawnSpeed;
+	public float ammoSpawnTimeout;
+
+	public GameObject ammoPrefab;
+
+	private int xRightBound = 16;
+	private int xLeftBound = -16;
+
+	private int yTopBound = 9;
+	private int yBottomBound = -9;
+
+	// Use this for initialization
+	void Start () {
+		ammoSpawnSpeed = 5.0F;
+		ammoSpawnTimeout = Time.time + ammoSpawnSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > ammoSpawnTimeout) {
+			SpawnAmmo ();
+			ammoSpawnTimeout = Time.time + ammoSpawnSpeed;
+		}
+	}
+
+	void SpawnAmmo() { 
+		int xSpawn = Random.Range (xLeftBound, xRightBound);
+		int ySpawn = Random.Range (yBottomBound, yTopBound);
+		Vector3 spawnLocation = new Vector3 (xSpawn, ySpawn, 0);
+		Instantiate (ammoPrefab, spawnLocation, new Quaternion ());
+	}
+}
