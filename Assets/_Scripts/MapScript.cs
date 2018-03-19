84a11f3d0242abe040cdc27a410546fa97ab8fc4
Marco Cunqueiro <cunqueim@mit.edu>
2018-03-19 15:24:33 -0400

    Adding mapscript

---
+++ Assets/_Scripts/MapScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour {
+
+	private SpriteRenderer spriteR;
+
+	// Use this for initialization
+	void Start () {
+		spriteR = gameObject.GetComponent<SpriteRenderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeToBlack() {
+		print ("changing color");
+		spriteR.color = new Color(0.0f, 0.0f, 0.0f);
+	}
+
+	public void changeToWhite() {
+		spriteR.color = Color.white;
+	}
+}
