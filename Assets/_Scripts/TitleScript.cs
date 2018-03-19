0750b3ab86cbc617db6d7404b18919fb6da2fcf6
David Rosales <anekin@mit.edu>
2018-03-19 15:44:41 -0400

    Export settings and screen resolution set

--- Assets/_Scripts/TitleScript.cs
+++ Assets/_Scripts/TitleScript.cs
@@ -7,7 +7,7 @@ public class TitleScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Screen.SetResolution (1500, 900, true);
 	}
 	
 	// Update is called once per frame
